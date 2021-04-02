68fea0d846e4bb538aa39f032df7912237849a9d
Cody Michaud <cody.r.michaud@gmail.com>
2021-04-01 18:53:28 -0700

    Websockets commit 1

---
+++ server.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const express = require('express');
+const { Server } = require('ws');
+
+const PORT = process.env.PORT || 3000;
+const INDEX = '/index.html';
+
+const server = express().use((req, res) => res.sendFile(INDEX, { root: __dirname })).listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+const wss = new Server({ server });
+
+wss.on('connection', (ws) => {
+    console.log('Client Connected');
+    ws.on('close', () => console.log('Client Disconnected'));
+});
+
+setInterval(() => {
+    wss.clients.forEach((client) => {
+        client.send(new Date().toTimeString());
+    });
+}, 1000);
